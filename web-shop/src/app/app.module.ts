f2a0c9b053969d07cb248c49312e4b08ffa35a11
burakerdgn1 <burakerdogan749@gmail.com>
2023-08-22 22:08:20 +0300

    Angular material installed.
    -Relevant imports are added.

--- web-shop/src/app/app.module.ts
+++ web-shop/src/app/app.module.ts
@@ -3,6 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import  {MatSidenavModule} from '@angular/material/sidenav';
+import  {MatGridListModule} from '@angular/material/grid-list';
+import  {MatMenuModule} from '@angular/material/menu';
+import  {MatButtonModule} from '@angular/material/button';
+import  {MatCardModule} from '@angular/material/card';
+import  {MatIconModule} from '@angular/material/icon';
+import  {MatExpansionModule} from '@angular/material/expansion';
+import  {MatListModule} from '@angular/material/list';
+import  {MatToolbarModule} from '@angular/material/toolbar';
+import  {MatTableModule} from '@angular/material/table';
+import  {MatBadgeModule} from '@angular/material/badge';
+import  {MatSnackBarModule} from '@angular/material/snack-bar';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +25,22 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatListModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatBadgeModule,
+    MatSnackBarModule,
+  
+
   ],
   providers: [],
   bootstrap: [AppComponent]
